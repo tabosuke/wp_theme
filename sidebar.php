4133d492f337374ba98e2e301119255ae2dad09b
bamshot <tabosuke810@gmail.com>
2018-08-05 00:25:03 +0900

    2018/08/05 sp時ヘッダーの固定

--- sidebar.php
+++ sidebar.php
@@ -3,3 +3,8 @@
     <?php dynamic_sidebar( 'side-widget' ); ?>
   </div>
 </div>
+<div class="footerMenu_sp">
+  <div class="footerMenu__copyright">
+    <p>Copyright ©<?php bloginfo( 'name' ); ?> All Rights Reserved.</p>
+  </div>
+</div>
